8c4c60595cc98e7bbc2cd8a3605a63757999287a
wenpeng <676395159@qq.com>
2020-03-04 22:59:51 +0800

    第二天作业

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,17 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        int[] testArray ={1,2,2,3,5,5,6};
-        Solution solution = new Solution();
-        System.out.println(solution.removeDuplicates(testArray));
+//	    // 第一课
+//        int[] testArray ={1,2,2,3,5,5,6};
+//        Solution solution = new Solution();
+//        System.out.println(solution.removeDuplicates(testArray));
+//        System.out.println(Arrays.toString(testArray));
+        //第二课
+        Solution2 solution2 = new Solution2();
+        int[] testArray = {1,2};
+        solution2.rotate(testArray,3);
         System.out.println(Arrays.toString(testArray));
+
     }
 
 }
